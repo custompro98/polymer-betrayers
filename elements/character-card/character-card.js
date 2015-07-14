68f3aba79c2b94961fec29ddcb16f7ac3a20cd90
custompro98 <mitchjoa@gmail.com>
2015-07-14 11:27:02 -0400

    Update character-card.js

--- elements/character-card/character-card.js
+++ elements/character-card/character-card.js
@@ -100,7 +100,7 @@ Polymer('character-card', {
 		}		
 	},
 
-	clickHandler: function(event) {
+	clickHandler: function(event, detail, sender) {
 		attribute = event.target.attributes['name'].value;
 		direction = event.target.attributes['class'].value;
 		self.updateStat(attribute, direction);
