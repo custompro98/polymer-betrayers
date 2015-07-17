673cd72363c18c20f324e293cdd22176958e7572
custompro98 <mitchjoa@gmail.com>
2015-07-17 15:51:32 -0400

    Update character-card.js

--- elements/character-card/character-card.js
+++ elements/character-card/character-card.js
@@ -1,61 +1,154 @@
-<link rel="import" href="../../bower_components/polymer/polymer.html">
-<link rel="import" href="../../bower_components/paper-header-panel/paper-header-panel.html">
-<link rel="import" href="../../bower_components/iron-icons/iron-icons.html">
-<link rel="import" href="../../bower_components/iron-icon/iron-icon.html">
+Polymer({
+	is: "character-card",
 
-<dom-module id="character-card">
-    <link rel="import" type="css" href="character-card.css">
+	properties: {
+		character: Object,
+		stats: {
+			type: Object,
+			notify: true
+		}
+	},
 
-    <template>
-        <paper-header-panel>
-            <div class="paper-header" id="characterName" on-click="fullscreen">{{character.Name}}</div>
-            <div class="content" id="characterStats">
-                <div id="profilePic">
-                </div>
-                <div id="vitals">
-                    <div class="trait">
-                        <span class="category">Age: </span><span id="age">{{character.Age}}</span>
-                    </div>
-                    <div class="trait">
-                        <span class="category">Height: </span><span id="height">{{character.Height}}</span>
-                    </div>
-                    <div class="trait">
-                        <span class="category">Weight: </span><span id="weight">{{character.Weight}}</span>
-                    </div>
-                    <div class="trait">
-                        <span class="category">Birthday: </span><span id="birthday">{{character.Birthday}}</span>
-                    </div>
-                </div>
-                <div class="trait" id="tHobbies">
-                    <span class="category">Hobbies: </span><span id="hobbies">{{character.Hobbies}}</span>
-                </div>
-                <div id="physical">
-                    <div class="attribute">
-                        <iron-icon icon="expand-less" direction="up" name="might" on-click="clickHandler"></iron-icon><br/>
-                        (<span id="mightIndex">{{stats.MightIndex::value-changed}}</span>)<span class="category">Might: </span>{{stats.MightScore}}<span id="mightScore"></span><br/>
-                        <iron-icon icon="expand-more" direction="down" name="might" on-click="clickHandler"></iron-icon>
-                    </div>
-                    <div class="attribute">
-                        <iron-icon icon="expand-less" direction="up" name="speed" on-click="clickHandler"></iron-icon><br/>
-                        (<span id="speedIndex">{{stats.SpeedIndex}}</span>)<span class="category">Speed: </span>{{stats.SpeedScore}}<span id="speedScore"></span><br/>
-                        <iron-icon icon="expand-more" direction="down" name="speed" on-click="clickHandler"></iron-icon>
-                    </div>
-                </div>
-                <div id="mental">
-                    <div class="attribute">
-                        <iron-icon icon="expand-less" direction="up" name="knowledge" on-click="clickHandler"></iron-icon><br/>
-                        (<span id="knowledgeIndex">{{stats.KnowledgeIndex}}</span>)<span class="category">Knowledge: </span>{{stats.KnowledgeScore}}<span id="knowledgeScore"></span><br/>
-                        <iron-icon icon="expand-more" direction="down" name="knowledge" on-click="clickHandler"></iron-icon>
-                    </div>
-                    <div class="attribute">
-                        <iron-icon icon="expand-less" direction="up" name="sanity" on-click="clickHandler"></iron-icon><br/>
-                        (<span id="sanityIndex">{{stats.SanityIndex}}</span>)<span class="category">Sanity: </span>{{stats.SanityScore}}<span id="sanityScore"></span><br/>
-                        <iron-icon icon="expand-more" direction="down" name="sanity" on-click="clickHandler"></iron-icon>
-                    </div>
-                </div>
-            </div>
-        </paper-header-panel>
-    </template>
+	fullscreen: function() {
+		screenfull.toggle();
+	},
 
-    <script src="character-card.js"></script>
-</dom-moduid
+	updateStat: function(attribute, direction) {
+		switch(attribute) {
+			case "might":
+				switch(direction) {
+					case "up":
+						console.log("up");
+						if(stats.MightIndex < 8) {
+							stats.MightScore = stats.Might[++stats.MightIndex];
+						}
+						break;
+					case "down":
+						console.log("down");
+						if(stats.MightIndex > 0) {
+							stats.MightScore = stats.Might[--stats.MightIndex];
+						}
+						break;
+				}
+				if(!stats.MightIndex) {
+					alert("You've died.");
+				}
+				break;
+			case "speed":
+				switch(direction) {
+					case "up":
+						console.log("up");
+						if(stats.SpeedIndex < 8) {
+							stats.SpeedScore = stats.Speed[++stats.SpeedIndex];
+						}
+						break;
+					case "down":
+						console.log("down");
+						if(stats.SpeedIndex > 0) {
+							stats.SpeedScore = stats.Speed[--stats.SpeedIndex];
+						}
+						break;
+				}
+				if(!stats.SpeedIndex) {
+					alert("You've died.");
+				}
+				break;
+			case "knowledge":
+				switch(direction) {
+					case "up":
+						console.log("up");
+						if(stats.KnowledgeIndex < 8) {
+							stats.KnowledgeScore = stats.Knowledge[++stats.KnowledgeIndex];
+						}
+						break;
+					case "down":
+						console.log("down");
+						if(stats.KnowledgeIndex > 0) {
+							stats.KnowledgeScore = stats.Knowledge[--stats.KnowledgeIndex];
+						}
+						break;
+				}
+				if(!stats.KnowledgeIndex) {
+					alert("You've died.");
+				}
+				break;
+			case "sanity":
+				switch(direction) {
+					case "up":
+						console.log("up");
+						if(stats.SanityIndex < 8) {
+							stats.SanityScore = stats.Sanity[++stats.SanityIndex];
+						}
+						break;
+					case "down":
+						console.log("down");
+						if(stats.SanityIndex > 0) {
+							stats.SanityScore = stats.Sanity[--stats.SanityIndex];
+						}
+						break;
+				}
+				if(!stats.SanityIndex) {
+					alert("You've died.");
+				}
+				break;
+		}
+
+		this.stats = stats;	
+	},
+
+	clickHandler: function(event, detail) {
+		attribute = event.target.attributes['name'].value;
+		direction = event.target.attributes['direction'].value;
+		self.updateStat(attribute, direction);
+	},
+
+	setHeader: function(color) {
+		pattern = /^\#([a-f]|[0-9]){6}$$/i;
+		if(!pattern.test(color)) {
+			color = "#000000";
+		}
+
+		this.$.characterName.style.backgroundColor = color;
+	},
+
+	setCardValues: function() {
+		selection = this.character;
+		this.setHeader(selection.Color);
+
+		stats.MightScore = selection.Might[selection.BaseMightIndex];
+		stats.SpeedScore = selection.Speed[selection.BaseSpeedIndex];
+		stats.KnowledgeScore = selection.Knowledge[selection.BaseKnowledgeIndex];
+		stats.SanityScore = selection.Sanity[selection.BaseSanityIndex];	
+
+		stats.Might = selection.Might;
+		stats.Speed = selection.Speed;
+		stats.Knowledge = selection.Knowledge;
+		stats.Sanity = selection.Sanity;
+		stats.MightIndex = selection.BaseMightIndex
+		stats.SpeedIndex = selection.BaseSpeedIndex
+		stats.KnowledgeIndex = selection.BaseKnowledgeIndex
+		stats.SanityIndex = selection.BaseSanityIndex
+	},
+
+	attached: function() {
+		self = this;
+
+		stats = {
+			"Might":"",
+			"Speed":"",
+			"Knowledge":"",
+			"Sanity":"",
+			"MightIndex":"",
+			"SpeedIndex":"",
+			"KnowledgeIndex":"",
+			"SanityIndex":"",
+			"MightScore":"",
+			"SpeedScore":"",
+			"KnowledgeScore":"",
+			"SanityScore":""
+		};
+
+		this.setCardValues();
+		this.stats = stats;
+	},
+});
